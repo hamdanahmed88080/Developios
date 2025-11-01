f6921907f0f31719095f49cc2ea8eed992e552f0
Hamdan Ahmed <hamdanahmed@gmail.com>
2025-11-01 21:03:47 +0500

    Animations working. Need some adjustmet. Responsivness left

--- src/Components/Services/Service/Service.js
+++ src/Components/Services/Service/Service.js
@@ -1,30 +1,92 @@
 import styles from "./Service.module.css";
 
+import arrowRightSvg from "../../../images/arrow-right-top.svg";
+
+import serviceHoverImage from "../../../images/service-hover-image.svg";
+
 import { useInView } from "react-intersection-observer";
 
-const Service = () => {
+const Service = ({ id }) => {
   const { ref, inView, entry } = useInView({
     triggerOnce: true,
     threshold: 0,
   });
 
+  function onMouseEnterHandler(event) {
+    var currentTarget = event.currentTarget;
+    // console.log(currentTarget.children);
+    var targetChildren = currentTarget.children;
+    var arrowRightDiv = targetChildren[2].children[0];
+    var img = targetChildren[2].children[0].children[0];
+    // var img = targetChildren[2].children[0];
+    // console.log();
+    if (arrowRightDiv && img) {
+      arrowRightDiv.classList.toggle(styles.buttonHover);
+      // img.style.color = "white";
+      img.style.color = "white";
+    }
+    // currentTarget.children.forEach((element) => {
+    //   console.log(element);
+    // });
+    var element = document.getElementsByClassName("Service_hoverImage__nNC2D");
+    var scalableImage = currentTarget.lastElementChild;
+
+    if (scalableImage) {
+      scalableImage.classList.toggle(styles.hoverImageScale);
+    }
+  }
+
+  function onMouseLeaveHandler(event) {
+    var currentTarget = event.currentTarget;
+    var scalableImage = currentTarget.lastElementChild;
+    var targetChildren = currentTarget.children;
+    var arrowRightDiv = targetChildren[2].children[0];
+    var img = targetChildren[2].children[0].children[0];
+    if (arrowRightDiv && img) {
+      arrowRightDiv.classList.toggle(styles.buttonHover);
+      // img.style.color = "white";
+      img.style.color = "black";
+    }
+
+    if (scalableImage) {
+      scalableImage.classList.toggle(styles.hoverImageScale);
+    }
+  }
+
   return (
-    <div>
+    <div style={{ display: "grid" }}>
       <a>
         <div
-          id="service1"
+          id={id}
           ref={ref}
           className={`${styles.serviceDiv} ${
             inView ? styles.serviceSlideIn : ""
           }`}
+          onMouseEnter={onMouseEnterHandler}
+          onMouseLeave={onMouseLeaveHandler}
         >
-          <h2>UX CRO Edit</h2>
+          <h2>
+            <span>
+              UX CRO<br></br>
+            </span>{" "}
+            Audit
+          </h2>
           <p>
             It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout.0
+            the readable content of a page when looking at its layout.
           </p>
-          <button></button>
-          <div className={styles.buttonText}>Learn More</div>
+          <div className={styles.buttonArea}>
+            <div className={styles.arrowRightSvg}>
+              <p>
+                <i
+                  className="fa fa-arrow-right"
+                  style={{ fontSize: "20px" }}
+                ></i>
+              </p>
+            </div>
+            <p>Learn More</p>
+          </div>
+          <img className={styles.hoverImage} src={serviceHoverImage}></img>
         </div>
       </a>
     </div>
--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -22,34 +22,26 @@ const Services = () => {
   return (
     <div className={styles.servicesMain}>
       <div className={styles.servicesHp}>
-        <div
-          ref={ref}
-          className={`${styles.servicesInnerDiv} ${
-            inView ? styles.slideIn : ""
-          }`}
-        >
-          <ul>
-            <li className={`${styles.servicesHpHeading}`}>
-              <h2>SERVICES</h2>
-            </li>
-            <li className={`${styles.servicesHpPara}`}>
-              <p>
-                From initial design to ongoing management, our team delivers
-                tailored solutions that enhance user experience, boost
-                conversions, and drive growth. Explore our core offerings below
-                and see how we can help you.
-              </p>
-            </li>
-          </ul>
+        <div className={`${styles.servicesHpHeading}`}>
+          <h2>SERVICES</h2>
+        </div>
+
+        <div className={`${styles.servicesHpPara}`}>
+          <p>
+            From initial design to ongoing management, our team delivers
+            tailored solutions that enhance user experience, boost conversions,
+            and drive growth. Explore our core offerings below and see how we
+            can help you.
+          </p>
         </div>
       </div>
       <div className={styles.servicesSection}>
-        <Service></Service>
-        <Service></Service>
-        <Service></Service>
-        <Service></Service>
-        <Service></Service>
-        <Service></Service>
+        <Service id="service1"></Service>
+        <Service id="service2"></Service>
+        <Service id="service3"></Service>
+        <Service id="service4"></Service>
+        <Service id="service5"></Service>
+        <Service id="service6"></Service>
       </div>
     </div>
   );
